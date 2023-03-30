a8cdd129764da0421b8b62c226ac5bdc0fe6f8c2
Bhupender <kbhupender455@gmail.com>
2023-03-30 11:27:16 +0530

    Third page working now

--- app/src/main/java/com/example/myapplication/LeagueActivity.kt
+++ app/src/main/java/com/example/myapplication/LeagueActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+    fun leagueButtonClicked(view:View){
+        val skillActivity = Intent(this,SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
